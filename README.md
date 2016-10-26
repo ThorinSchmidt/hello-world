9542eeb71b7ac3a525485c3c0c019e793aab4545
Thorin Schmidt <tschmidt@debian00.cis.com>
2016-10-26 10:15:12 -0500

    added helloworld.py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hello-world
 Just a test
+
+CHANGELOG
+added  helloworld.py - 10/26/2016
---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+# HelloWorld.py
+# Thorin Schmidt
+# 10/26/2016
+
+""" Program that tells the world 'Hello'"""
+
+print("Hello, World!")
